9db42890de7a67072eafbf78660d9adeee83eaf5
Sharang Goel <goelshaz001@gmail.com>
2025-01-19 19:22:47 -0500

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="./favicon.ico"/>
+        <meta property="og:title" content="Sharang Goel's Portfolio" />
+        <meta property="og:description" content="Check out my portfolio made with Next.js, Tailwind CSS, TypeScript, and Aceternity UI." />
+        <meta property="og:image" content="https://www.sharang-goel.dev/" />
+        <meta property="og:url" content="https://www.sharang-goel.dev" />
+        <meta property="og:type" content="website" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#01020f]`}>
         {children}
